c3180c6b19c1c29f346f4d7cce935501016568f6
NaRa <naropticon@outlook.com>
2025-07-11 03:53:15 +0200

    Update clone-my-all-repos.sh
    https://www.youtube.com/watch?v=2GpdpfgZqt0&list=RD2GpdpfgZqt0&start_radio=1
    
    :)))

--- clone-my-all-repos.sh
+++ clone-my-all-repos.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-GITLAB_TOKEN="SAJÁT_TOKENED_IDE"
+GITLAB_TOKEN="Authorization: Bearer {TOKEN}"
 GITLAB_API_URL="https://gitlab.com/api/v4/projects?membership=true&per_page=100"
 CLONE_DIR="./gitlab-repos"
 
 mkdir -p "$CLONE_DIR"
 
-curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" -s "$GITLAB_API_URL" |
+curl -s --insecure --location --request GET "$GITLAB_TOKEN" --header "$GITLAB_API_URL" |
   jq -r '.[].ssh_url_to_repo' |
   while read -r repo_url; do
     echo "Klónozás: $repo_url"
